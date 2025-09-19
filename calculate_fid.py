37ebd9e4cd1aa83fd1eec3ca5261e11a46547d8c
Stephen Huan <stephen.huan@cgdct.moe>
2025-09-19 07:47:38 -0400

    feat(calculate_fid.py): add fid computation script

---
+++ calculate_fid.py
@@ -0,0 +1,57 @@
+import argparse
+import subprocess
+from pathlib import Path
+
+# meant to be used with the edm2 repository (https://github.com/NVlabs/edm2)
+
+
+def compute_metrics(
+    gpus: int,
+    *,
+    images: Path,
+    ref: str,
+    metrics: str,
+    outdir: Path,
+) -> None:
+    out = subprocess.run(
+        [
+            "torchrun",
+            "--standalone",
+            f"--nproc_per_node={gpus}",
+            "calculate_metrics.py",
+            "calc",
+            f"--images={images}",
+            f"--ref={ref}",
+            f"--metrics={metrics}",
+        ],
+        capture_output=True,
+        text=True,
+        check=True,
+    )
+    print(out.stdout)
+    outdir.mkdir(exist_ok=True, parents=True)
+    with open(outdir / f"{images.name}.txt", "w") as f:
+        f.write(out.stdout)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--gpus", type=int)
+    parser.add_argument("--images", type=Path)
+    parser.add_argument("--ref", type=str)
+    parser.add_argument("--metrics", type=str)
+    parser.add_argument("--outdir", type=Path, default=Path("fid"))
+    args = parser.parse_args()
+
+    dirnames = []
+    for _, dirnames, _ in args.images.walk():
+        break
+
+    for dirname in sorted(dirnames):
+        compute_metrics(
+            gpus=args.gpus,
+            images=args.images / dirname,
+            ref=args.ref,
+            metrics=args.metrics,
+            outdir=args.outdir,
+        )
